966290f05d21d1783b7fa6ecb1c1a4f107244d09
jra-gh <jayson.ra.wallace@pm.me>
2026-01-08 15:15:38 -0600

    Test gemini response

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from agentic-code-buddy!")
+import os
 
+from dotenv import load_dotenv
 
-if __name__ == "__main__":
-    main()
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+from google import genai
+
+client = genai.Client(api_key=api_key)
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+)
+print(response.text)
